a6a67be72f6750b63a5c9f15c17b55744b045bef
TheBeardedOne <jeremy.boggs42@gmail.com>
2014-01-18 09:52:22 -0500

    Fixed Bug

--- Assets/stairTriggerScript.cs
+++ Assets/stairTriggerScript.cs
@@ -16,13 +16,16 @@ public class stairTriggerScript : MonoBehaviour {
 	void OnTriggerEnter(Collider other){
 
 		if(other.tag == "Player" && gameObject.name == "stairTrigger"){
-			GameObject.FindGameObjectWithTag("stairObject").GetComponent<stepsMovement>().moved = 0.0f;
-			GameObject.FindGameObjectWithTag("stairObject").GetComponent<stepsMovement>().activated = 0;
+			if(GameObject.FindGameObjectWithTag("stairObject").GetComponent<stepsMovement>().activated != 0){
+			   GameObject.FindGameObjectWithTag("stairObject").GetComponent<stepsMovement>().moved = 0.0f;
+			   GameObject.FindGameObjectWithTag("stairObject").GetComponent<stepsMovement>().activated = 0;
+			}
 		}
 		if(other.tag == "Player" && gameObject.name == "stairTriggerOff"){
+			if(GameObject.FindGameObjectWithTag("stairObject").GetComponent<stepsMovement>().activated != 1){
 			GameObject.FindGameObjectWithTag("stairObject").GetComponent<stepsMovement>().moved = 0.0f;
 			GameObject.FindGameObjectWithTag("stairObject").GetComponent<stepsMovement>().activated = 1;
-
+			}
 		}
 
 	}
