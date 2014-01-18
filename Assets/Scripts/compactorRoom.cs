a92e0fcfcd764a0dac8d40d499e44c5c71469e5e
Tarwine <tarwine0406@gmail.com>
2014-01-18 01:52:09 -0500

    Compactor room tweaks

--- Assets/Scripts/compactorRoom.cs
+++ Assets/Scripts/compactorRoom.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 
 public class compactorRoom : MonoBehaviour {
+	public bool activated;
+	public GameObject leftWall;
+	public GameObject rightWall;
 
 	// Use this for initialization
 	void Start () {
-	
+		activated = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(activated){
+			leftWall.rigidbody.AddForce(10000,0,0);
+			activated = false;
+		}
 	}
 }
