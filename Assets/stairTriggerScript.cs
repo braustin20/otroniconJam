9cb74f477b36bfd2d0e6b4a39e70f37e018ffe7e
TheBeardedOne <jeremy.boggs42@gmail.com>
2014-01-18 09:00:47 -0500

    STEPS!

---
+++ Assets/stairTriggerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class stairTriggerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+
+		if(other.tag == "Player" && gameObject.name == "stairTrigger"){
+			GameObject.FindGameObjectWithTag("stairObject").GetComponent<stepsMovement>().moved = 0.0f;
+			GameObject.FindGameObjectWithTag("stairObject").GetComponent<stepsMovement>().activated = 0;
+		}
+		if(other.tag == "Player" && gameObject.name == "stairTriggerOff"){
+			GameObject.FindGameObjectWithTag("stairObject").GetComponent<stepsMovement>().moved = 0.0f;
+			GameObject.FindGameObjectWithTag("stairObject").GetComponent<stepsMovement>().activated = 1;
+
+		}
+
+	}
+}
