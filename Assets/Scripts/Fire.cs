39745ef2af35cadd0172030bea1eea9cf062326e
Travis <tfort34@gmail.com>
2014-01-19 13:00:22 -0500

    piece

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		StartCoroutine("LifeCycle");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator LifeCycle() {
+		yield return new WaitForSeconds(30 - transform.position.y + 25);
+		Destroy (this.gameObject);
+	}
+}
