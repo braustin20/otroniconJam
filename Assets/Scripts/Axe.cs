d384eac0b71783f5da74c3623ae7182eabb7315e
Travis <tfort34@gmail.com>
2014-01-18 23:32:32 -0500

    basic axe implementation

---
+++ Assets/Scripts/Axe.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Axe : MonoBehaviour {
+
+	JointMotor motor;
+	float timer;
+	int velocityMod;
+
+	// Use this for initialization
+	void Start () {
+		motor = this.hingeJoint.motor;
+		timer = 2f;
+		velocityMod = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+
+		if (timer <= 0) {
+			velocityMod *= -1;
+			timer = 2f;
+		}
+
+		motor.targetVelocity = 500 * velocityMod * Mathf.Cos ((timer / 2));
+
+		this.hingeJoint.motor = motor;
+
+	}
+}
