d8a1b268570f816b8646ba515cd3cdea227e2894
Travis <tfort34@gmail.com>
2014-01-19 08:53:13 -0500

    Set

---
+++ Assets/Scripts/SetPiece.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetPiece : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.AddForce(Vector3.up * 100);
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
